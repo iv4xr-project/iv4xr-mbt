104c8becbeae277d41ae4a6975c0c98df1362d41
Fitsum Kifetew <fitsumm@gmail.com>
2021-01-20 10:07:33 +0100

    added utils for serializing/deserializing tests to disk, mainly for testing/debugging purposes

---
+++ src/main/java/eu/fbk/iv4xr/mbt/utils/TestSerializationUtils.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package eu.fbk.iv4xr.mbt.utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import org.apache.commons.lang3.SerializationUtils;
+
+import eu.fbk.iv4xr.mbt.testcase.AbstractTestSequence;
+
+/**
+ * @author kifetew
+ *
+ */
+public class TestSerializationUtils {
+
+	/**
+	 * 
+	 */
+	public TestSerializationUtils() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public static AbstractTestSequence loadTestSequence (String filename) throws FileNotFoundException {
+		AbstractTestSequence ts = (AbstractTestSequence) SerializationUtils.deserialize(new FileInputStream(filename));
+		return ts;
+	}
+	
+	public static void saveTestSequence (AbstractTestSequence test, String filename) throws FileNotFoundException {
+		SerializationUtils.serialize(test, new FileOutputStream(filename));
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
