0e6baef4762b950e84d64632c65753304989ea8d
Davide Prandi <prandi@fbk.eu>
2021-04-27 10:53:25 +0200

    Implemented LabRecruit test suite executor

--- src/test/java/eu/fbk/iv4xr/mbt/labrecruitsexecutor/ServerTest.java
+++ src/test/java/eu/fbk/iv4xr/mbt/labrecruitsexecutor/ServerTest.java
@@ -1,15 +1,15 @@
 package eu.fbk.iv4xr.mbt.labrecruitsexecutor;
 
 import static nl.uu.cs.aplib.AplibEDSL.SEQ;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.LinkOption;
 import java.nio.file.Paths;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -20,6 +20,7 @@ import agents.LabRecruitsTestAgent;
 import agents.tactics.GoalLib;
 import environments.LabRecruitsConfig;
 import environments.LabRecruitsEnvironment;
+
 import eu.fbk.iv4xr.mbt.MBTProperties;
 import eu.fbk.iv4xr.mbt.efsm.EFSM;
 import eu.fbk.iv4xr.mbt.efsm.EFSMFactory;
@@ -27,12 +28,17 @@ import eu.fbk.iv4xr.mbt.efsm.EFSMState;
 import eu.fbk.iv4xr.mbt.efsm.EFSMTransition;
 import eu.fbk.iv4xr.mbt.execution.EFSMTestExecutor;
 import eu.fbk.iv4xr.mbt.execution.ExecutionResult;
+import eu.fbk.iv4xr.mbt.execution.labrecruits.LabRecruitsTestSuiteExecutor;
+import eu.fbk.iv4xr.mbt.strategy.GenerationStrategy;
+import eu.fbk.iv4xr.mbt.strategy.SearchBasedStrategy;
 import eu.fbk.iv4xr.mbt.testcase.AbstractTestSequence;
+import eu.fbk.iv4xr.mbt.testcase.MBTChromosome;
 import eu.fbk.iv4xr.mbt.testcase.Path;
 import eu.fbk.iv4xr.mbt.testcase.RandomLengthTestFactory;
 import eu.fbk.iv4xr.mbt.testcase.Testcase;
+import eu.fbk.iv4xr.mbt.testsuite.SuiteChromosome;
 import eu.iv4xr.framework.mainConcepts.TestAgent;
-import eu.iv4xr.framework.mainConcepts.WorldEntity;
+import eu.iv4xr.framework.mainConcepts.TestDataCollector;
 import game.LabRecruitsTestServer;
 import game.Platform;
 
@@ -61,25 +67,23 @@ public class ServerTest {
 	public void createAndLoadLevelTest() throws InterruptedException, IOException {
 		
 		
-		// basic configurations
+		// basic configurations of LR
 		String levelName = "testLRexecutor";
 		String levelPath = Paths.get(Platform.LEVEL_PATH, levelName).toString();
 		String agentName = "Agent1";
 		String levelFileName = levelName + "_LR";
+		String labRecruitesExeRootDir = System.getProperty("user.dir");
 
 		// MBT configurations
 		MBTProperties.LR_generation_mode = MBTProperties.LR_random_mode.N_BUTTONS_DEPENDENT;
 		MBTProperties.LR_n_rooms = 5;
 		MBTProperties.LR_n_doors = 4;
-		
-		
-		// use the factory to create a level
+		MBTProperties.SEARCH_BUDGET = 60;
 		MBTProperties.SUT_EFSM = "labrecruits.random_default";
-		EFSMFactory factory = EFSMFactory.getInstance(true);
 		
-		// create random level with default parameters
+		// create random level and save it
+		EFSMFactory factory = EFSMFactory.getInstance(true);
 		EFSM efsm = factory.getEFSM();
-
 		// try to save the csv. If the csv is not generated the test fails
 		if (efsm.getEFSMString() == "") {
 			System.out.println("Cannot create a planar graph with these paratemers");
@@ -91,142 +95,33 @@ public class ServerTest {
 		    writer.close();
 		}
 
-		// open the level with LabRectuits
-		String labRecruitesExeRootDir = System.getProperty("user.dir");
-		System.out.println(labRecruitesExeRootDir);
-		LabRecruitsTestServer testServer = new LabRecruitsTestServer(false,
-				Platform.PathToLabRecruitsExecutable(labRecruitesExeRootDir));
-
-		LabRecruitsConfig lrCfg = new LabRecruitsConfig(levelFileName);
-		var environment = new LabRecruitsEnvironment(lrCfg);
-
-		LabRecruitsTestAgent agent = new LabRecruitsTestAgent(agentName).attachState(new BeliefState())
-				.attachEnvironment(environment);
-
-		// get a feasible test case
-		Boolean feasible = false;
-		Testcase testcase = null;
-		while( ! feasible) {
-			RandomLengthTestFactory testFactory = new RandomLengthTestFactory(efsm);
-			testcase = testFactory.getTestcase();
-			ExecutionResult result = EFSMTestExecutor.getInstance().executeTestcase(testcase);
-			if (result.isSuccess()) {
-				feasible = true;
-			}
-		}
-		
-		// convert the test case ot a goal structure
-		GoalStructure goals = convertTestCaseToGoalStructure(agent,(AbstractTestSequence) testcase);
-		
-		if (goals.getSubgoals().size() > 0) {
-			agent.setGoal(goals);
-
-	        // press play in Unity
-	        if (! environment.startSimulation())
-	            throw new InterruptedException("Unity refuses to start the Simulation!");
-
-	        int i = 0 ;
-	        while (goals.getStatus().inProgress()) {
-	            agent.update();
-	            System.out.println("*** " + i + "/" 
-	               + agent.getState().worldmodel.timestamp + ", "
-	               + agent.getState().id + " @" + agent.getState().worldmodel.position) ;
-	            Thread.sleep(5);
-	            i++ ;
-	            if (i>200) {
-	            	break ;
-	            }
-	        }
-	        
-	        goals.printGoalStructureStatus();
-		}else {
-			System.out.println("Cannot build a goal structure from test case ");
-			System.out.println(toString());
-			fail();
-		}
-	
-		
-
-		// close the server
-		if (testServer != null)
-			testServer.close();
-
-	}
-	
-	
-	// sample code for translating EFSM test case into a goal structure
-	private GoalStructure convertTestCaseToGoalStructure(TestAgent agent, AbstractTestSequence tc) {
-		Path path = tc.getPath();
-		List<EFSMTransition> listTransitions = path.getTransitions();
-		List<GoalStructure> subGoals = new LinkedList<GoalStructure>() ;
-		for(EFSMTransition t : listTransitions) {
-			LinkedList<GoalStructure> transitionGoals = convertEFMSTransitionToGoal(agent, t);
-			for(GoalStructure g : transitionGoals) {
-				subGoals.add(g);
-			}			
-		}
-		
-		GoalStructure testingTask = SEQ(subGoals.toArray(new GoalStructure[0])) ;
-		return testingTask;
-	}
-	
-	
-	private LinkedList<GoalStructure> convertEFMSTransitionToGoal(TestAgent agent, EFSMTransition t ) {
-		LinkedList<GoalStructure> subGoals = new LinkedList<GoalStructure>() ;
-		// start refreshing the origin state
-		//subGoals.add(GoalLib.entityStateRefreshed(convertStateToString(t.getSrc())));
-		
-		// look at src and tgt state to understand the type of transition
-		if (t.getSrc().equals(t.getTgt())) {
-			// if self loop we are pressing a button
-			subGoals.add(GoalLib.entityInteracted(t.getTgt().getId())) ;
-		}else if (oppositeDoorSides(t.getSrc(),t.getTgt())) {
-			// to optimize
-			String doorName = convertDoorSideToDoorName(t.getTgt().getId());
+		// compute a solution with MOSA
+		GenerationStrategy generationStrategy = new SearchBasedStrategy<MBTChromosome>();
+		SuiteChromosome solution = generationStrategy.generateTests();
 			
-			subGoals.add(GoalLib.entityStateRefreshed(doorName));
-  			//subGoals.add(GoalLib.entityInvariantChecked(agent, doorName, doorName+" should be open", (WorldEntity e) -> e.getBooleanProperty("isOpen"))) ;
-  		}else {
-  			subGoals.add(GoalLib.entityStateRefreshed(convertStateToString(t.getTgt()))) ;
-  		}
+		// create the executor
+        LabRecruitsTestSuiteExecutor lrExecutor = new LabRecruitsTestSuiteExecutor(labRecruitesExeRootDir, levelFileName, agentName);
+        
+        // execute the test suite
+        LinkedHashMap<AbstractTestSequence, Boolean> response = lrExecutor.executeTestSuite(solution);
+        
+        // check the results
+        System.out.println("Performed "+response.size()+" test");
+        for(AbstractTestSequence test : response.keySet()) {     	
+        	if (response.get(test)) {
+        		//System.out.println("PASS");
+        		//System.out.println(test.toString());
+        	}else {
+        		System.out.println("FAIL");
+        		System.out.println(test.toString());
+        		//fail();
+        	}
+        }
+
 		
-		return subGoals;
+
 	}
 	
-	private String convertStateToString(EFSMState s) {
-		if (isDoor(s)) {
-			return convertDoorSideToDoorName(s.getId());
-		}else {
-			return s.getId();
-		}
-	}
-	
-	// take a string in the form d1p/d1m and return door1
-	private String convertDoorSideToDoorName(String doorSide) {
-		String tmp = doorSide.substring( 1, doorSide.length() - 1 );
-		return "door"+tmp;
-	}
-	
-	private Boolean isDoor(EFSMState s) {
-		String name = s.getId();
-		String b = name.substring(0, 1);
-		if (name.substring(0, 1).equals("b")) {
-			return false;
-		}else {
-			return true;
-		}
-	}
-	
-	private Boolean oppositeDoorSides(EFSMState s, EFSMState t) {
-		if (!isDoor(s) || !isDoor(t)) {
-			// one of the state is not a door
-			return false;
-		}else if (convertDoorSideToDoorName(s.toString()).equals(convertDoorSideToDoorName(t.toString())) &&
-				s.toString() != t.toString()){
-			return true;
-		}else {
-			return false;
-		}
-	}
+
 	
 }
