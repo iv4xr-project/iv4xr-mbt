c6a9a7ce62fb9f356e273066dd3e79950a349ca8
Fitsum Kifetew <fitsumm@gmail.com>
2021-05-14 11:37:59 +0200

    disabling tamer

--- src/main/java/eu/fbk/iv4xr/mbt/strategy/PlanningBasedStrategy.java
+++ src/main/java/eu/fbk/iv4xr/mbt/strategy/PlanningBasedStrategy.java
@@ -46,14 +46,14 @@ import eu.fbk.iv4xr.mbt.efsm.EFSMFactory;
 import eu.fbk.iv4xr.mbt.testcase.MBTChromosome;
 import eu.fbk.iv4xr.mbt.testsuite.MBTSuiteChromosome;
 import eu.fbk.iv4xr.mbt.testsuite.SuiteChromosome;
-import eu.fbk.jtamer.jtamer;
-import eu.fbk.jtamer.tamer_action;
-import eu.fbk.jtamer.tamer_env;
-import eu.fbk.jtamer.tamer_expr;
-import eu.fbk.jtamer.tamer_problem;
-import eu.fbk.jtamer.tamer_ttplan;
-import eu.fbk.jtamer.tamer_ttplan_step;
-import eu.fbk.jtamer.tamer_ttplan_step_iter;
+//import eu.fbk.jtamer.jtamer;
+//import eu.fbk.jtamer.tamer_action;
+//import eu.fbk.jtamer.tamer_env;
+//import eu.fbk.jtamer.tamer_expr;
+//import eu.fbk.jtamer.tamer_problem;
+//import eu.fbk.jtamer.tamer_ttplan;
+//import eu.fbk.jtamer.tamer_ttplan_step;
+//import eu.fbk.jtamer.tamer_ttplan_step_iter;
 
 /**
  * Iteratively generate random tests. If adding the random test
@@ -77,8 +77,6 @@ public class PlanningBasedStrategy<T extends Chromosome> extends GenerationStrat
 		
 		Properties.LOG_LEVEL = "WARN";
 		Properties.SEARCH_BUDGET = MBTProperties.SEARCH_BUDGET;
-		
-//		MBTProperties.SUT_EFSM = "labrecruits.random_simple";
 	}
 	
 	@Override
@@ -145,62 +143,63 @@ public class PlanningBasedStrategy<T extends Chromosome> extends GenerationStrat
 	}
 	
 	
+	// Uncomment contents to use with tamer
 	private void callPlanner (String anmlFilePath) {
-		tamer_env env = jtamer.tamer_env_new();
-
-        // create the problem parsing an ANML file
-        tamer_problem problem = jtamer.tamer_parse_anml(env, anmlFilePath);
-
-        // solve the problem
-        jtamer.tamer_env_set_boolean_option(env, "tsimple-goals-serialization", 1);
-        jtamer.tamer_env_set_string_option(env, "tsimple-heuristic", "blind");
-        tamer_ttplan plan = jtamer.tamer_ttplan_from_potplan(jtamer.tamer_do_tsimple_planning(problem));
-
-        // read the plan
-        tamer_ttplan_step_iter steps = jtamer.tamer_ttplan_get_steps(plan);
-        for (int i=0; i<jtamer.tamer_ttplan_get_size(plan); i++) {
-                tamer_ttplan_step step = jtamer.tamer_ttplan_step_iter_next(steps);
-                tamer_action action = jtamer.tamer_ttplan_step_get_action(step);
-                String action_name = jtamer.tamer_action_get_name(action);
-                List<String> action_params = new ArrayList<String>();
-                for (int j=0; j<jtamer.tamer_ttplan_step_get_num_parameters(step); j++) {
-                        tamer_expr param = jtamer.tamer_ttplan_step_get_parameter(step, j);
-                        action_params.add(jtamer.tamer_expr_get_anml(param));
-                }
-                double duration = parse(jtamer.tamer_ttplan_step_get_duration(step));
-                double start_time = parse(jtamer.tamer_ttplan_step_get_start_time(step));
-                System.out.println(start_time + " : " + action_name + "(" + String.join(", ", action_params) + ") [" + duration + "]");
-        }
+//		tamer_env env = jtamer.tamer_env_new();
+//
+//        // create the problem parsing an ANML file
+//        tamer_problem problem = jtamer.tamer_parse_anml(env, anmlFilePath);
+//
+//        // solve the problem
+//        jtamer.tamer_env_set_boolean_option(env, "tsimple-goals-serialization", 1);
+//        jtamer.tamer_env_set_string_option(env, "tsimple-heuristic", "blind");
+//        tamer_ttplan plan = jtamer.tamer_ttplan_from_potplan(jtamer.tamer_do_tsimple_planning(problem));
+//
+//        // read the plan
+//        tamer_ttplan_step_iter steps = jtamer.tamer_ttplan_get_steps(plan);
+//        for (int i=0; i<jtamer.tamer_ttplan_get_size(plan); i++) {
+//                tamer_ttplan_step step = jtamer.tamer_ttplan_step_iter_next(steps);
+//                tamer_action action = jtamer.tamer_ttplan_step_get_action(step);
+//                String action_name = jtamer.tamer_action_get_name(action);
+//                List<String> action_params = new ArrayList<String>();
+//                for (int j=0; j<jtamer.tamer_ttplan_step_get_num_parameters(step); j++) {
+//                        tamer_expr param = jtamer.tamer_ttplan_step_get_parameter(step, j);
+//                        action_params.add(jtamer.tamer_expr_get_anml(param));
+//                }
+//                double duration = parse(jtamer.tamer_ttplan_step_get_duration(step));
+//                double start_time = parse(jtamer.tamer_ttplan_step_get_start_time(step));
+//                System.out.println(start_time + " : " + action_name + "(" + String.join(", ", action_params) + ") [" + duration + "]");
+//        }
 	}
 	
-	private void __callPlanner (String anmlFilePath) {
-		tamer_env env = jtamer.tamer_env_new();
-		
-		
-		// =================================================================
-				
-		// create the problem parsing an ANML file
-		tamer_problem problem = jtamer.tamer_parse_anml(env, anmlFilePath); //"lab_recruits_random_1.anml");
-		System.out.println("Loaded problem file: " + anmlFilePath);
-		// solve the problem
-		tamer_ttplan plan = jtamer.tamer_ttplan_from_potplan(jtamer.tamer_do_iw_planning(problem));
-		
-		// read the plan
-		tamer_ttplan_step_iter steps = jtamer.tamer_ttplan_get_steps(plan);
-		for (int i=0; i<jtamer.tamer_ttplan_get_size(plan); i++) {
-			tamer_ttplan_step step = jtamer.tamer_ttplan_step_iter_next(steps);
-			tamer_action action = jtamer.tamer_ttplan_step_get_action(step);
-			String action_name = jtamer.tamer_action_get_name(action);
-			List<String> action_params = new ArrayList<String>();
-			for (int j=0; j<jtamer.tamer_ttplan_step_get_num_parameters(step); j++) {
-				tamer_expr param = jtamer.tamer_ttplan_step_get_parameter(step, j);
-				action_params.add(jtamer.tamer_expr_get_anml(param));
-			}
-			double duration = parse(jtamer.tamer_ttplan_step_get_duration(step));
-			double start_time = parse(jtamer.tamer_ttplan_step_get_start_time(step));
-			System.out.println(start_time + " :: " + action_name + "(" + String.join(", ", action_params) + ") [" + duration + "]");
-		}
-	}
+//	private void __callPlanner (String anmlFilePath) {
+//		tamer_env env = jtamer.tamer_env_new();
+//		
+//		
+//		// =================================================================
+//				
+//		// create the problem parsing an ANML file
+//		tamer_problem problem = jtamer.tamer_parse_anml(env, anmlFilePath); //"lab_recruits_random_1.anml");
+//		System.out.println("Loaded problem file: " + anmlFilePath);
+//		// solve the problem
+//		tamer_ttplan plan = jtamer.tamer_ttplan_from_potplan(jtamer.tamer_do_iw_planning(problem));
+//		
+//		// read the plan
+//		tamer_ttplan_step_iter steps = jtamer.tamer_ttplan_get_steps(plan);
+//		for (int i=0; i<jtamer.tamer_ttplan_get_size(plan); i++) {
+//			tamer_ttplan_step step = jtamer.tamer_ttplan_step_iter_next(steps);
+//			tamer_action action = jtamer.tamer_ttplan_step_get_action(step);
+//			String action_name = jtamer.tamer_action_get_name(action);
+//			List<String> action_params = new ArrayList<String>();
+//			for (int j=0; j<jtamer.tamer_ttplan_step_get_num_parameters(step); j++) {
+//				tamer_expr param = jtamer.tamer_ttplan_step_get_parameter(step, j);
+//				action_params.add(jtamer.tamer_expr_get_anml(param));
+//			}
+//			double duration = parse(jtamer.tamer_ttplan_step_get_duration(step));
+//			double start_time = parse(jtamer.tamer_ttplan_step_get_start_time(step));
+//			System.out.println(start_time + " :: " + action_name + "(" + String.join(", ", action_params) + ") [" + duration + "]");
+//		}
+//	}
 	
 	private double parse(String ratio) {
 	    if (ratio.contains("/")) {
