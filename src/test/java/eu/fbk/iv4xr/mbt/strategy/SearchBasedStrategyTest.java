49641708e5c1210a6b9f5a6f6eddd5413a40cbe9
Davide <davide.prandi@gmail.com>
2021-12-06 10:34:56 +0100

    Add test SearchBasedStrategyTest.java

---
+++ src/test/java/eu/fbk/iv4xr/mbt/strategy/SearchBasedStrategyTest.java
@@ -0,0 +1,61 @@
+package eu.fbk.iv4xr.mbt.strategy;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+
+import eu.fbk.iv4xr.mbt.MBTProperties;
+import eu.fbk.iv4xr.mbt.MBTProperties.ModelCriterion;
+import eu.fbk.iv4xr.mbt.efsm.EFSM;
+import eu.fbk.iv4xr.mbt.efsm.EFSMFactory;
+import eu.fbk.iv4xr.mbt.efsm.EFSMState;
+import eu.fbk.iv4xr.mbt.testcase.AbstractTestSequence;
+import eu.fbk.iv4xr.mbt.testcase.MBTChromosome;
+import eu.fbk.iv4xr.mbt.testsuite.SuiteChromosome;
+
+/**
+ * 
+ * @author prandi
+ *
+ */
+public class SearchBasedStrategyTest {
+
+	@Test
+	public void testGenerateTests() {
+		// Model criterion should be STATE
+		MBTProperties.MODELCRITERION = new ModelCriterion[] {
+				ModelCriterion.STATE 
+		};
+	
+		MBTProperties.LR_n_goalFlags = 2;
+		
+		EFSMFactory mFactory = EFSMFactory.getInstance(true);
+		assertNotNull(mFactory);
+		EFSM efsm = mFactory.getEFSM();
+		assertNotNull (efsm);
+		
+		EFSMState gf0 = new EFSMState("gf0");
+		assertTrue(efsm.getStates().contains(gf0));
+		
+		Set<String> targetSet = new HashSet<>();
+		targetSet.add("gf0");
+		
+		SearchBasedStrategy sbStrategy = new SearchBasedStrategy<>();
+		SuiteChromosome generatedTests = sbStrategy.generateTests(targetSet);
+		List<MBTChromosome> testChromosomes = generatedTests.getTestChromosomes();
+		
+		System.out.println("\nGenerated "+testChromosomes.size()+" test cases");
+		for(MBTChromosome chr : testChromosomes) {
+			AbstractTestSequence testcase = (AbstractTestSequence) chr.getTestcase();
+			System.out.println("Fitness: "+testcase.getFitness());
+			System.out.println(testcase.toString());
+			
+		}
+		
+	}
+}
