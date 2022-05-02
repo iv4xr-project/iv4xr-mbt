2d26706c284ed511d1e5fa2a29440abed1a5b9df
ukarakaya1 <ukarakaya@fbk.eu>
2022-05-02 11:36:43 +0200

    Pending State

--- src/main/java/eu/fbk/iv4xr/mbt/efsm/examples/TrafficLight.java
+++ src/main/java/eu/fbk/iv4xr/mbt/efsm/examples/TrafficLight.java
@@ -6,12 +6,10 @@ import eu.fbk.iv4xr.mbt.efsm.EFSMContext;
 import eu.fbk.iv4xr.mbt.efsm.EFSMGuard;
 import eu.fbk.iv4xr.mbt.efsm.EFSMOperation;
 import eu.fbk.iv4xr.mbt.efsm.EFSMParameter;
-import eu.fbk.iv4xr.mbt.efsm.EFSMParameterGenerator;
 import eu.fbk.iv4xr.mbt.efsm.EFSMState;
 import eu.fbk.iv4xr.mbt.efsm.EFSMTransition;
 import eu.fbk.iv4xr.mbt.efsm.exp.Assign;
 import eu.fbk.iv4xr.mbt.efsm.exp.Const;
-import eu.fbk.iv4xr.mbt.efsm.exp.Exp;
 import eu.fbk.iv4xr.mbt.efsm.exp.Var;
 import eu.fbk.iv4xr.mbt.efsm.exp.bool.BoolAnd;
 import eu.fbk.iv4xr.mbt.efsm.exp.bool.BoolNot;
@@ -21,47 +19,41 @@ import eu.fbk.iv4xr.mbt.efsm.exp.integer.IntGreat;
 import eu.fbk.iv4xr.mbt.efsm.exp.integer.IntSum;
 import eu.fbk.iv4xr.mbt.efsm.labRecruits.LRParameterGenerator;
 
-/**
- * Traffic light EFSM model from 
- * https://ptolemy.berkeley.edu/projects/chess/eecs149/lectures/ExtendedAndTimedAutomata.pdf
- * slide 3 
- * @author prandi
- *
- */
-
 public class TrafficLight {
-
+	
+	
+	public enum outSignal{ sigR, sigG, sigY, sigP };
+	
+	
+	//// States
 	// four states
 	public EFSMState red = new EFSMState("Red");
 	public EFSMState yellow = new EFSMState("Yellow");
 	public EFSMState green = new EFSMState("Green");
 	public EFSMState pending = new EFSMState("Pending");
 	
-	// context 
+	
+	//// context 
 	public Var<Integer> count = new Var<Integer>("count", 0);
+	public EFSMContext tlContext = new EFSMContext(count);
 	
-	public Const<Boolean> pedestrian = new Const<Boolean>(true);
+	//// input
+	private Var<Boolean> pedestrian = new Var<Boolean>("pedestrian",true);
+
+	//// output 
+	public Var<Enum> signal = new Var<Enum>("signal", outSignal.sigR);
+	//public EFSMParameter output = new EFSMParameter(signal);
+
 	
-<<<<<<< Updated upstream
-	EFSMContext tlContext = new EFSMContext(count);
-=======
-	//public Var<Integer> pedestrian = new Var<Integer>("pedestrian", 60);
-	
-	
-	EFSMContext tlContext = new EFSMContext(count);
-	EFSMContext tlContext2 = new EFSMContext(pedestrian);
-	
->>>>>>> Stashed changes
-	
-	// operation increment count
+	//// operations
+	// increment count
 	Assign<Integer> incCount = new Assign(count, new IntSum(count, new Const(1)));
+	// reset count
 	Assign<Integer> resetCount = new Assign(count, new Const<Integer>(0));
 	
-<<<<<<< Updated upstream
-=======
+
+	//// checks to be used in guards
 	
->>>>>>> Stashed changes
-	// check counter values
 	// constant 60
 	Const<Integer> sixty = new Const<Integer>(60);
 	// count greater that 60
@@ -73,225 +65,139 @@ public class TrafficLight {
 	// count less than 60
 	BoolNot countLessThanSixty = new BoolNot(countGreatEqThanSixty);
 	
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
-	// Constant 5
-	
+	// constant 5
 	Const<Integer> five = new Const<Integer>(5);
-	// count greater that 5
-	IntGreat countGreatThanFive =  new IntGreat(count, five);
-	// count equal 5
-	IntEq countEqualFive =  new IntEq(count, five);
-	// count greater or equal 5
-	BoolOr countGreatEqThanFive = new BoolOr(countGreatThanFive, countEqualFive);
-	// count less than 5
-	BoolNot countLessThanFive = new BoolNot(countGreatEqThanFive);
-	
-<<<<<<< Updated upstream
-	 
-	// Pedestrian
-	
-	//Const<Boolean> pedestrian = new Const<Boolean>(true);
-	
-	
 	// count greater that 60
-	//IntGreat p_countGreatThanSixty =  new IntGreat(count, pendingSixty);
+	IntGreat countGreatThanFive = new IntGreat(count, five);
 	// count equal 60
-	//IntEq p_countEqualSixty =  new IntEq(count, pendingSixty);
+	IntEq countEqualfive = new IntEq(count, five);
 	// count greater or equal 60
-	//BoolOr p_countGreatEqThanSixty = new BoolOr(p_countGreatThanSixty, p_countEqualSixty);
+	BoolOr countGreatEqThanFive = new BoolOr(countGreatThanFive, countEqualfive);
 	// count less than 60
-	//BoolNot p_countLessThanSixty = new BoolNot(p_countGreatEqThanSixty);
-	BoolAnd checkPoint = new BoolAnd(countLessThanSixty,pedestrian);
+	BoolNot countLessThanFive = new BoolNot(countGreatEqThanFive);
+		
+	// with pedestrian
+	BoolAnd countGreatEqThanSixtyAndPedestrian = new BoolAnd(countGreatEqThanSixty,pedestrian);
+	BoolAnd countLessThanSixtyAndPedestrian = new BoolAnd(countLessThanSixty,pedestrian);
 	
-	
-	// Green is less than 60 and pedestrian is less than 60 //
-	//BoolOr pending_status_less = new BoolOr(p_countLessThanSixty,countLessThanSixty);
-=======
-	
-	// Green is less than 60 and pedestrian is less than 60 //
-	Exp<Boolean> pending_input = new Const<Boolean>(true);
-	BoolAnd pending_status_less = new BoolAnd(pending_input,countLessThanSixty);
-	BoolNot pending_status_greater = new BoolNot(pending_status_less);
->>>>>>> Stashed changes
-	
-	// define transition guards
-	EFSMGuard guardCountGreatEqThanSixty = new EFSMGuard(countGreatEqThanSixty);
-	EFSMGuard guardCountLessThanSixty = new EFSMGuard(countLessThanSixty);
-	
-<<<<<<< Updated upstream
-	
-	EFSMGuard pending_check = new EFSMGuard(checkPoint);
-=======
-	//EFSMGuard guardPedGreatEqThanSixty = new EFSMGuard(countGreatEqThanSixty);
-	EFSMGuard guardPedLessThanSixty = new EFSMGuard(pending_status_less);
-	EFSMGuard guardPedGreaterThanSixty = new EFSMGuard(pending_status_greater);
-	
-	
-	//EFSMGuard pending_check = new EFSMGuard(checkPoint);
->>>>>>> Stashed changes
-	
-	EFSMGuard guardCountGreatEqThanFive = new EFSMGuard(countGreatEqThanFive);
-	EFSMGuard guardCountLessThanFive = new EFSMGuard(countLessThanFive);
-	
-	// define transition operations
-<<<<<<< Updated upstream
-	EFSMOperation operationIncCount_red = new EFSMOperation(incCount);
-	EFSMOperation operationIncCount_green = new EFSMOperation(incCount);
-	EFSMOperation operationIncCount_yellow = new EFSMOperation(incCount);
-	
-	EFSMOperation operationResetCount_red = new EFSMOperation(resetCount);
-	EFSMOperation operationResetCount_green = new EFSMOperation(resetCount);
-	EFSMOperation operationResetCount_yellow = new EFSMOperation(resetCount);
-=======
-	EFSMOperation operationIncCount = new EFSMOperation(incCount);
-	//EFSMOperation operationIncCount_green = new EFSMOperation(incCount);
-	//EFSMOperation operationIncCount_yellow = new EFSMOperation(incCount);
-	//EFSMOperation operationIncCount_pedestrian = new EFSMOperation(incPedestrian);
-	
-	EFSMOperation operationResetCount = new EFSMOperation(resetCount);
-	//EFSMOperation operationResetCount_green = new EFSMOperation(resetCount);
-	//EFSMOperation operationResetCount_yellow = new EFSMOperation(resetCount);
-	//EFSMOperation operationResetCount_pedestrian = new EFSMOperation(resetPedestrian);
->>>>>>> Stashed changes
-	
-	// inputs and outputs
-	// input are local to transitions 
-	// - boolean variable pedestrian that check if pedestrian button has been pushed
-	
-	// output are local to transition
-	// - enumerator for light color
-	
+	public Var<Boolean> getPedestrian() {
+		return pedestrian;
+	}
 
+	public void setPedestrian(Var<Boolean> pedestrian) {
+		this.pedestrian = pedestrian;
+	}
+	
 	public TrafficLight() {
 		
 	}
 	
 	public EFSM getModel() {
+	
+		//// Define transition
 		
 		// t_0 : red -> red # increment count
-		EFSMTransition t_0 = new EFSMTransition<>();
-		t_0.setGuard(guardCountLessThanSixty);
-<<<<<<< Updated upstream
-		t_0.setOp(operationIncCount_red);
-=======
-		t_0.setOp(operationIncCount);
->>>>>>> Stashed changes
+		EFSMTransition t_0 = new EFSMTransition();
+		t_0.setGuard(new EFSMGuard(countLessThanSixty));
+		t_0.setOp(new EFSMOperation(incCount));
 		
-		// t_1 : red -> green 
-		EFSMTransition t_1 = new EFSMTransition<>();
-		t_1.setGuard(guardCountGreatEqThanSixty);  
-<<<<<<< Updated upstream
-		t_1.setOp(operationResetCount_red);
-=======
-		t_1.setOp(operationResetCount);
->>>>>>> Stashed changes
+		// t_1 : red -> green
+		EFSMTransition t_1 = new EFSMTransition();
+		t_1.setGuard(new EFSMGuard(countGreatEqThanSixty));
+		t_1.setOp(new EFSMOperation(resetCount));
+		Var<Enum> t1Out = new Var<Enum>("signal", outSignal.sigG);
+		t_1.setOutParameter(new EFSMParameter(t1Out));
 		
-
-		
-		
-		// t_2 : yellow -> yellow # increment count
-		EFSMTransition t_2 = new EFSMTransition<>();
-		t_2.setGuard(guardCountLessThanFive);
-<<<<<<< Updated upstream
-		t_2.setOp(operationIncCount_yellow);
-=======
-		t_2.setOp(operationIncCount);
->>>>>>> Stashed changes
-		
-		// t_3 : yellow -> red 
-		EFSMTransition t_3 = new EFSMTransition<>();
-		t_3.setGuard(guardCountGreatEqThanFive);  
-<<<<<<< Updated upstream
-		t_3.setOp(operationResetCount_yellow);
-=======
-		t_3.setOp(operationResetCount);
->>>>>>> Stashed changes
-		
-		
-		// t4: green -> green
-		
-		EFSMTransition t_4 = new EFSMTransition<>();
-
-<<<<<<< Updated upstream
-		//t_4.setGuard(guardCountLessThanSixty);  
-		t_4.setOp(operationIncCount_green);
-		
-		// t5: green -> yellow with pending
-		
-	EFSMTransition  t_5 = new EFSMTransition<>();
-		t_5.setGuard(pending_check); 
-		t_5.setOp(operationResetCount_green);
-		
-		
-		
-		/*
-=======
-		t_4.setGuard(guardCountLessThanSixty);  
-		t_4.setOp(operationIncCount);
-		
-		// t5: green -> yellow with pedestrian
-		
-		EFSMTransition  t_5 = new EFSMTransition<>();
-		t_5.setGuard(guardPedGreaterThanSixty);
-		t_5.setOp(operationResetCount);
-		
-		
-		// t6: green -> pending 
-		
-		EFSMTransition  t_6 = new EFSMTransition<>();
-		t_6.setGuard(guardPedLessThanSixty);
-		t_6.setOp(operationResetCount);
-		
-		
-		// t7: pending -> pending 
-		
-		EFSMTransition  t_7 = new EFSMTransition<>();
-		t_7.setGuard(guardCountGreatEqThanSixty);  
-		t_7.setOp(operationResetCount);
-		
+		// t_2 : green -> green
+		EFSMTransition t_2 = new EFSMTransition();
+		t_2.setGuard(new EFSMGuard(countLessThanSixty));
+		t_2.setOp(new EFSMOperation(incCount));
 				
-		// t8: pending -> yellow 
-				
-		EFSMTransition  t_8 = new EFSMTransition<>();
-		//t_7.setGuard(pending_check); 
-		t_8.setOp(operationResetCount);
-				
-				
+		// t_3 : green -> yellow 
+		EFSMTransition t_3 = new EFSMTransition();
+		// input
+		Var<Boolean> t3In = new Var<Boolean>("pedestrian",true);		
+		t_3.setInParameter(new EFSMParameter(t3In));
+		// guard
+		t_3.setGuard(new EFSMGuard(countGreatEqThanSixtyAndPedestrian));
+		// operation
+		t_3.setOp(new EFSMOperation(resetCount));
+		// output
+		Var<Enum> t3Out = new Var<Enum>("signal", outSignal.sigY);
+		t_3.setOutParameter(new EFSMParameter(t3Out));
 		
-		/*
-		 * 
->>>>>>> Stashed changes
-		 * EFSM declaration
-		 */
+		
+		
+		// t_4 : green -> pending
+		EFSMTransition t_4 = new EFSMTransition();
+		// input
+		Var<Boolean> t4In = new Var<Boolean>("pedestrian",true);		
+		t_4.setInParameter(new EFSMParameter(t4In));
+		// guard
+		t_4.setGuard(new EFSMGuard(countLessThanSixtyAndPedestrian));
+		
+		t_4.setOp(new EFSMOperation(resetCount));
+		
+		Var<Enum> t4Out = new Var<Enum>("signal", outSignal.sigP);
+		t_4.setOutParameter(new EFSMParameter(t4Out));
+		
+		// no operation and no output
+		
+		
+		
+		
+		// t_5 : yellow -> yellow # increment count
+		EFSMTransition t_5 = new EFSMTransition();
+		t_5.setGuard(new EFSMGuard(countLessThanFive));
+		t_5.setOp(new EFSMOperation(incCount));
+		
+		// t_6 : yellow -> red 
+		EFSMTransition t_6 = new EFSMTransition();
+		t_6.setGuard(new EFSMGuard(countGreatEqThanFive));
+		t_6.setOp(new EFSMOperation(resetCount));
+		Var<Enum> t6Out = new Var<Enum>("signal", outSignal.sigR);
+		t_6.setOutParameter(new EFSMParameter(t6Out));
+		
+		// t_7 : pending -> pending # increment count
+		EFSMTransition t_7 = new EFSMTransition();
+		t_7.setGuard(new EFSMGuard(countLessThanSixty));
+		t_7.setOp(new EFSMOperation(incCount));
+		
+		
+		// t_8 : pending -> yellow 
+		EFSMTransition t_8 = new EFSMTransition();
+		t_8.setGuard(new EFSMGuard(countGreatEqThanSixty));
+		t_8.setOp(new EFSMOperation(resetCount));
+		Var<Enum> t8Out = new Var<Enum>("signal", outSignal.sigY);
+		t_8.setOutParameter(new EFSMParameter(t6Out));
+		
+		
+		
+		
+		//// The model and the associated builder
 		EFSM trafficLightEFSM;
-	
-	
-	    EFSMBuilder trafficLightEFSMBuilder = new EFSMBuilder(EFSM.class);
-		
-	    
-	    // parameter generator
 
-	    LRParameterGenerator lrParameterGenerator = new LRParameterGenerator();
-	    
-	    trafficLightEFSM = trafficLightEFSMBuilder
+		EFSMBuilder trafficLightEFSMBuilder = new EFSMBuilder(EFSM.class);
+
+		// parameter generator 
+		// FIXME
+		LRParameterGenerator lrParameterGenerator = new LRParameterGenerator();
+		
+		
+		trafficLightEFSM = trafficLightEFSMBuilder
 	    		.withTransition(red, red, t_0)
 	    		.withTransition(red, green, t_1)
-	    		.withTransition(yellow, yellow, t_2)
-	    		.withTransition(yellow, red, t_3)
-	    		.withTransition(green, green, t_4)
-	    		.withTransition(green, yellow, t_5)
-<<<<<<< Updated upstream
-=======
-	    		.withTransition(green, pending, t_6)
+	    		.withTransition(green, green, t_2)
+	    		.withTransition(green, yellow, t_3)
+	    		.withTransition(green, pending, t_4)
+	    		.withTransition(yellow, yellow, t_5)
+	    		.withTransition(yellow, red, t_6)
 	    		.withTransition(pending, pending, t_7)
 	    		.withTransition(pending, yellow, t_8)
->>>>>>> Stashed changes
 	    		.build(red,tlContext, lrParameterGenerator);
 	    
 	    return(trafficLightEFSM);
 	}
+	
 	
 }
--- src/test/java/eu/fbk/iv4xr/mbt/efsm/TrafficLightTest.java
+++ src/test/java/eu/fbk/iv4xr/mbt/efsm/TrafficLightTest.java
@@ -33,36 +33,21 @@ public class TrafficLightTest {
 		m.transition(null, tl.green);
 		assertTrue(m.curState.equals(tl.red));
 		assertTrue((Integer)m.curContext.getContext().getVariable("count").getValue()<=60);	
-<<<<<<< Updated upstream
-		assertTrue(tl.pedestrian.getVal());	
-=======
-		//assertTrue(tl.pedestrian.getVal());	
->>>>>>> Stashed changes
-		
 		
 		m.transition(null, tl.yellow);
 		assertTrue(m.curState.equals(tl.red));	
 		assertFalse(m.curState.equals(tl.yellow));
-		assertTrue((Integer)m.curContext.getContext().getVariable("count").getValue()==0);	
-		
-<<<<<<< Updated upstream
-		
-		
+		assertTrue((Integer)m.curContext.getContext().getVariable("count").getValue()==0);		
 		
 	}
 	
 	
-=======
-				
-	}
-
->>>>>>> Stashed changes
-
 	@Test
 	public void testModel_2() {
 		TrafficLight tl = new TrafficLight();
 		EFSM m = tl.getModel();
 		
+		/*red --> red */
 		assertTrue(m.curState.equals(tl.red));	
 		assertTrue((Integer)m.curContext.getContext().getVariable("count").getValue()==0);
 		
@@ -71,44 +56,71 @@ public class TrafficLightTest {
 			m.transition(null, tl.red);
 		}
 		
+		/*red --> green */
+		
 		assertTrue(m.curState.equals(tl.red));			
 		assertTrue((Integer)m.curContext.getContext().getVariable("count").getValue()==60);
-				
 		m.transition(null, tl.green);
+		
+		/*green --> green */
 		assertTrue(m.curState.equals(tl.green));	
 		assertTrue((Integer)m.curContext.getContext().getVariable("count").getValue()==0);
-
-				
+		
 		for (int i = 0; i < 60; i++) {
 			assertTrue((Integer)m.curContext.getContext().getVariable("count").getValue()==i);
 			m.transition(null, tl.green);
 		}
-<<<<<<< Updated upstream
 		
+		
+		/*FROM GREEN TO YELLOW WITH PENDING STATE */
+		
+		/*green --> pending  pedestrian time should be less than 60 */
 		assertTrue(m.curState.equals(tl.green));
 		assertTrue((Integer)m.curContext.getContext().getVariable("count").getValue()<=60);	
-		assertTrue(tl.pedestrian.getVal());	
-		m.transition(null, tl.yellow);
-=======
-	
-		// green to pending
-		assertTrue(m.curState.equals(tl.green));
-		assertTrue((Integer)m.curContext.getContext().getVariable("count").getValue()<=60);
-		assertTrue((Boolean)m.curContext.getContext().getVariable("pedestrian").getValue()==true);		
+		assertTrue(tl.getPedestrian().getValue());
 		m.transition(null, tl.pending);
 		
-		// pending to pending
-		/*assertTrue(m.curState.equals(tl.pending));
-		for (int i = 1; i < 61; i++) {
-			assertTrue((Integer)m.curContext.getContext().getVariable("pedestrian").getValue()==i);
-			m.transition(null, tl.pending);
-		}*/
 		
-		//pending to yellow
-		//assertTrue(m.curState.equals(tl.pending));
-		//assertTrue((Integer)m.curContext.getContext().getVariable("count").getValue()==60);
-		//m.transition(null, tl.yellow);
->>>>>>> Stashed changes
+		/*pending --> pending */
+		assertTrue(m.curState.equals(tl.pending));
+		assertTrue((Integer)m.curContext.getContext().getVariable("count").getValue()==0);
+		
+		for (int i = 0; i < 60; i++) {
+			assertTrue((Integer)m.curContext.getContext().getVariable("count").getValue()==i);
+			m.transition(null, tl.pending);
+		}
+		
+		
+		/*pending --> yellow */
+		
+		assertTrue(m.curState.equals(tl.pending));			
+		assertTrue((Integer)m.curContext.getContext().getVariable("count").getValue()==60);
+		m.transition(null, tl.yellow);
+		
+		
+		/*FROM GREEN TO YELLOW WITHOUT PENDING STATE */
+
+		/*green --> yellow  pedestrian time should be greater than 60
+		assertTrue(m.curState.equals(tl.green));
+		assertTrue(tl.getTime_pedestrian().getValue()>=60);
+		assertTrue((Integer)m.curContext.getContext().getVariable("count").getValue()>=60);	
+		assertTrue(tl.getPedestrian().getValue());
+		m.transition(null, tl.yellow);*/
+		
+		/*yellow --> yellow */
+		assertTrue(m.curState.equals(tl.yellow));
+		assertTrue((Integer)m.curContext.getContext().getVariable("count").getValue()==0);
+		
+		for (int i = 0; i < 5; i++) {
+			assertTrue((Integer)m.curContext.getContext().getVariable("count").getValue()==i);
+			m.transition(null, tl.yellow);
+		}
+		
+		/*yellow --> red */
+		assertTrue(m.curState.equals(tl.yellow));			
+		assertTrue((Integer)m.curContext.getContext().getVariable("count").getValue()<=60);
+		m.transition(null, tl.red);
+
 		
 	}
 }
