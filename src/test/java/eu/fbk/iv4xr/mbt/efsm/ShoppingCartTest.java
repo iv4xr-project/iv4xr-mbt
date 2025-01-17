596149bfa929ddf129ba116bb04e83a5bdf1b66c
Pietro Lechthaler <plechthaler@fbk.eu>
2025-01-17 10:05:46 +0100

    added new example shoppingCart and its tests

---
+++ src/test/java/eu/fbk/iv4xr/mbt/efsm/ShoppingCartTest.java
@@ -0,0 +1,79 @@
+package eu.fbk.iv4xr.mbt.efsm;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import eu.fbk.iv4xr.mbt.efsm.examples.ShoppingCart;
+
+public class ShoppingCartTest {
+
+	@Test
+    public void testInitialState() {
+        ShoppingCart sc = new ShoppingCart();
+        EFSM m = sc.getModel();
+        
+        assertTrue(m.curState.equals(sc.amazon));
+        assertTrue((Integer)m.curContext.getContext().getVariable("numOfBooks").getValue() == 0);
+    }
+
+    @Test
+    public void testSearchAndAddBooks() {
+        ShoppingCart sc = new ShoppingCart();
+        EFSM m = sc.getModel();
+        
+        // Initially at Amazon base state
+        assertTrue(m.curState.equals(sc.amazon));
+        
+        // Transition to SearchResult via SearchBook event
+        m.transition(null, sc.searchResult);
+        assertTrue(m.curState.equals(sc.searchResult));
+        
+        //Book information
+        m.transition(null, sc.bookInformation);
+        assertTrue(m.curState.equals(sc.bookInformation));
+        
+        // Add book to cart, assume guard condition of numOfBooks <= 3 is met
+        m.transition(null, sc.addedToCart);
+        assertTrue(m.curState.equals(sc.addedToCart));
+        assertTrue((Integer)m.curContext.getContext().getVariable("numOfBooks").getValue() == 1);
+        
+        // Return to search results to add another book
+        m.transition(null, sc.searchResult);
+        assertTrue(m.curState.equals(sc.searchResult));
+        
+        // Try to add three more books
+        for (int i = 0; i < 3; i++) {
+            m.transition(null, sc.bookInformation);
+            assertTrue(m.curState.equals(sc.bookInformation));
+            m.transition(null, sc.addedToCart);
+            assertTrue(m.curState.equals(sc.addedToCart));
+        }
+        
+        // Check if the number of books is now 4
+        assertTrue((Integer)m.curContext.getContext().getVariable("numOfBooks").getValue() == 4);
+        
+        // Attempt to add another book, should fail because numOfBooks > 3
+        m.transition(null, sc.bookInformation);
+        assertTrue(m.curState.equals(sc.bookInformation));
+        m.transition(null, sc.addedToCart);
+        assertFalse(m.curState.equals(sc.addedToCart)); // No transition should happen
+        assertTrue((Integer)m.curContext.getContext().getVariable("numOfBooks").getValue() == 4);
+    }
+
+    @Test
+    public void testCheckoutProcess() {
+        ShoppingCart sc = new ShoppingCart();
+        EFSM m = sc.getModel();
+        
+        // Navigate to shopping cart from search results
+        m.transition(null, sc.searchResult);
+        assertTrue(m.curState.equals(sc.searchResult));
+        m.transition(null, sc.shoppingCart);
+        assertTrue(m.curState.equals(sc.shoppingCart));
+        
+        // Check out and return to search results
+        m.transition(null, sc.searchResult);
+        assertTrue(m.curState.equals(sc.searchResult));
+    }
+	
+}
